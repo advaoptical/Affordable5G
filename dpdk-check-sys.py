de0bbfd5fd21af08d37b37409dd9e315c5bc0dc0
andrewse <asergeev@advaoptical.com>
2021-10-27 13:19:13 +0300

    update from A5G repo

--- dpdk-check-sys.py
+++ dpdk-check-sys.py
@@ -6,7 +6,9 @@
 # This is the DPDK readyness checklist
 #  
 ##############################################
+from __future__ import print_function
 import os
+import platform
 
 
 def read_sys_info(fn):
@@ -18,42 +20,82 @@ def read_sys_info(fn):
     return s
 
 
-def check_cpu_freq():
-    cur_freq = read_sys_info('/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq')
-    max_freq = read_sys_info('/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq')
+def print_ddr_speed():
+    ddr_speed = 'DDR speed'
+    s = os.popen('dmidecode -t 17').read().split('\t')
+    for z in s:
+        if 'Speed' in z:
+            freq = z.split(':')[1].split()
+# ['1067', 'MT/s']
+# ['Unknown']
+            if len(freq) > 1:
+                break
 
-    if not cur_freq:
-        s = os.popen('dmidecode -t 4').read().split('\t')
-        for z in s:
-            if 'Max Speed' in z:
-                max_freq = z.split(':')[1].split()[0]
-            elif 'Current Speed:' in z:
-                cur_freq = z.split(':')[1].split()[0]
+    if (not freq):
+        print ("[error] Can't read %s" % (ddr_speed))
+        return
+
+    # DDR channel is encoded in letter:
+    # Locator: DIMM_A1   <-- A and B are different memory channels
+    # Locator: DIMM_B1
+    s = os.popen('dmidecode -t memory | grep Locator').read().split('\t')
+    n = 0
+    dimm = 'unknown'
+    for z in s:
+        if 'DIMM' in z:
+            tmp = z.split(':')[1]
+            if tmp[len(tmp)-3] != dimm[len(dimm)-3]:
+                n = n + 1
+                dimm = tmp
+
+    print ("[info] %s %s %s, %d channels" % (ddr_speed, freq[0], freq[1], n))
+
+
+def check_cpu_freq():
+    cur_freq = ''
+    max_freq = ''
+    cpu_type = '?'
+
+    s = os.popen('dmidecode -t 4').read().split('\t')
+    for z in s:
+        if 'Version' in z:
+            cpu_type = z.split(':')[1].rstrip('\n')
+        elif 'Max Speed' in z:
+            max_freq = z.split(':')[1].split()[0]
+        elif 'Current Speed:' in z:
+            cur_freq = z.split(':')[1].split()[0]
 
     if (not cur_freq) or (not max_freq):
-        print "[error] Can't read CPU frequency "
+        print ("[error] Can't read CPU frequency ")
         return
+
+    print ("[info] CPU%s" % (cpu_type))
 
     cur_freq = int(cur_freq)
     max_freq = int(max_freq)
 
     if cur_freq < max_freq:
-        print "[warning] CPU frequency %d is less than max %d" % (cur_freq, max_freq)
+        print ("[warning] CPU frequency %d is less than max %d" % (cur_freq, max_freq))
     else:
-        print "[OK] CPU frequency is max (%d MHz)" % (max_freq)
+        print ("[OK] CPU frequency is max (%d MHz)" % (max_freq))
+
 
 def check_cpu_scaling_governor():
     x = read_sys_info('/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor')
     if not x:
-        x = os.popen('tuned-adm active').read().split(':')[1]
+        try:
+            x = os.popen('tuned-adm active').read().split(':')[1].rstrip('\n') 
+        except:
+            pass
+
     if not x:
         x = '?'
 
     if 'performance' in x:
-        print "[OK] CPU scaling is %s" % x
+        print ("[OK] CPU scaling is", x)
     else:
-        print "[error] CPU scaling is %s" % x
-        print "\t please do: tuned-adm profile latency-performance"
+        print ("[error] CPU scaling is", x)
+        print ("\tconsider to do: tuned-adm profile latency-performance")
 
 
 def check_swap_enabled():
@@ -63,10 +105,10 @@ def check_swap_enabled():
         for line in f:
             if not line.startswith('#'):
                 if 'swap' in line:
-                    print "[error] swap is enabled in %s" % fn
+                    print ("[error] swap is enabled in %s" % fn)
                     return True
         
-    print "[OK] swap is disabled"
+    print ("[OK] swap is disabled")
     return False
 
 def check_huge_pages():
@@ -82,9 +124,9 @@ def check_huge_pages():
             sz = int(a[1])
                
     if n>0:
-        print "[OK] HugePages_Total: %d, %d kB each" % (n, sz)
+        print ("[OK] HugePages_Total: %d, %d kB each" % (n, sz))
     else:
-        print "[error] HugePages_Total: %d" % n
+        print ("[error] HugePages_Total: %d" % n)
 
 def check_transparent_huge_pages():
     # Based on the 3.10 kernel operating system, the transparent huge page function 
@@ -105,18 +147,18 @@ def check_transparent_huge_pages():
                     err_msg = 'OK'
                 break
                 
-    print "[%s] transparent_hugepage/enabled: %s" % (err_msg, x)
+    print ("[%s] transparent_hugepage/enabled: %s" % (err_msg, x))
 
     if err_msg != 'OK':
-        print "\t - please add transparent_hugepage=never to cmdline"
+        print ("\t - please add transparent_hugepage=never to cmdline")
 
 def check_service(service):
     stat = os.system('systemctl status '+service+' >/dev/null 2>&1')
     if stat != 0:
-        print '[OK] '+service+' not active'
+        print ('[OK] '+service+' not active')
     else:
-        print "[error] - please deactivate %s:\
-        \n\tsystemctl stop %s \n\tsystemctl disable %s" % (service,service,service)
+        print ("[error] - please deactivate %s:\
+        \n\tsystemctl stop %s \n\tsystemctl disable %s" % (service,service,service))
 
 #
 # isolcpus
@@ -136,14 +178,14 @@ def check_isol_cpu_cores():
         for index, y in enumerate(parms):
             if y in x:
                 a = x.split('=')
-                print "[OK] %s" % a
+                print ("[OK] %s" % a)
                 vals[index]=a[1]
 
     # print vals
     missed_parms = False
     for index, item in enumerate(vals):
         if item == "0":
-            print "[error] - please specify %s " % parms[index]
+            print ("[error] - please specify %s " % parms[index])
             missed_parms = True
 
     if missed_parms:
@@ -151,33 +193,7 @@ def check_isol_cpu_cores():
 
     result = all(element == vals[0] for element in vals)
     if not result:
-        print "[error] %s not %s not %s" % (vals[0], vals[1], vals[2])
-
-
-def check_pstate():
-    s = read_sys_info('/proc/cmdline').split()
-    for x in s:
-        if 'pstate' in x:
-            a=x.split('=')
-            if a[1] == 'disable':
-                print "[OK]", a
-            else:
-                print "[error] ", x
-            return
-    
-    print "[error] - please add intel_pstate=disable to cmdline"
-
-# 
-# Disables clocksource stability check interrupt for the Time Stamp Counter on all cores
-def check_tsc():
-    cfg = 'tsc=reliable'
-    s = read_sys_info('/proc/cmdline').split()
-    for x in s:
-        if cfg in x:
-            print "[OK]", cfg
-            return
-    
-    print "[error] - please add %s to cmdline" % cfg
+        print ("[error] %s not %s not %s" % (vals[0], vals[1], vals[2]))
 
 
 # Depending on your system, you'll find it in any one of these:
@@ -195,10 +211,10 @@ def check_linux_config(cfg):
                 s = os.popen('cat '+ fn).read()
 
             if cfg in s:
-                print "[OK]", cfg
+                print ("[OK]", cfg)
                 return
 
-    print "[error] - %s not found" % cfg
+    print ("[error] - %s not found" % cfg)
 
 def check_hyperthreading():
     s = os.popen('lscpu').read().split('\n')
@@ -209,25 +225,68 @@ def check_hyperthreading():
             break
 
     if n>1:
-        print "[warning] - HT enabled"
+        print ("[warning] - HT enabled")
     else:
-        print "[OK] HT disabled"
+        print ("[OK] HT disabled")
+
+def is_centos():
+    return platform.dist()[0] == 'centos'
+
+
+def is_selinux_enabled():
+    s = os.popen('sestatus').read()
+    return 'disabled' in s
+
+
+def check_cmdline(cfg):
+    s = read_sys_info('/proc/cmdline').split()
+    for x in s:
+        if cfg in x:
+            print ("[OK]", cfg)
+            return
+    
+    print ("[error] - please add %s to cmdline" % cfg)
+
+
 
 if __name__ == "__main__":
 
     print
     msg = "# Checking DPDK system readyness, kernel: "+os.uname()[2]
-    print msg
-    print "="*len(msg)
+    print (msg)
+    print('\t', platform.dist())
+    print ("="*len(msg))
+    check_cpu_freq()
+    print_ddr_speed()
     check_hyperthreading()
     check_swap_enabled()
     check_huge_pages()
     check_isol_cpu_cores()
-    check_pstate()
-    check_tsc()
-    check_cpu_scaling_governor()
-    check_cpu_freq()
-    check_transparent_huge_pages()
+    # Completely eliminate timer interrupts on a set of cores
     check_linux_config('CONFIG_NO_HZ_FULL=y')
+    check_cmdline('nohz=on')
+
+    # Disables clocksource stability check interrupt for the Time Stamp Counter on all cores
+    check_cmdline('tsc=reliable')
+
+    if is_centos():
+        if is_selinux_enabled():
+            print ("[OK] selinux disabled")
+        else:
+            check_cmdline('selinux=0')
+            check_cmdline('enforcing=0')
+
+    check_cmdline('intel_pstate=disable')
+    check_cmdline('nmi_watchdog=0')
+    check_cmdline('audit=0')
+    check_cmdline('mce=off')
+    check_cmdline('kthread_cpus=0')
+    check_cmdline('irqaffinity=0')
+    check_cmdline('skew_tick=1')
+    check_cmdline('nosoftlockup')
+    check_cpu_scaling_governor()
+    check_transparent_huge_pages()
     check_service('irqbalance')
     check_service('impi')
+    check_service('ipmievd')
+
